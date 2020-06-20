39725644141ea3215ef6bb8a145efb641faf4c69
Ulrik Johansson <ulrik.johansson@blocket.se>
2020-06-20 17:43:28 +0200

    output flux pub key to be added to githubZ

--- install.sh
+++ install.sh
@@ -8,3 +8,10 @@ kubectx microk8s
 kubectl cluster-info
 kubectl apply -k fluxcd
 kubectl -n flux rollout status deployment/flux
+
+# get the key to be used as deployment key for github
+FLUX_PUB_KEY=$(fluxctl --k8s-fwd-ns flux identity)
+echo "Add this to the flux repo deployment keys:"
+echo ""
+echo $FLUX_PUB_KEY
+
